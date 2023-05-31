cfe1b6dabddaf83908e20bc52a13613092e60989
blacksou <7042phpdev>
2023-05-31 03:27:58 +0300

    add routes for rate,subscribe and sendEmails GS-2

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,21 @@
 <?php
+
+use App\Controllers\EmailController;
+use App\Controllers\RateController;
+
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+if ($requestUri === '/rate' && $requestMethod === 'GET') {
+    $rateController = new RateController();
+    $rateController->rate();
+} elseif ($requestUri === '/subscribe' && $requestMethod === 'POST') {
+    $emailController = new EmailController();
+    $emailController->subscribe();
+} elseif ($requestUri === '/sendEmails' && $requestMethod === 'POST') {
+    $emailController = new EmailController();
+    $emailController->emails();
+} else {
+    http_response_code(404);
+    echo 'Not Found';
+}
