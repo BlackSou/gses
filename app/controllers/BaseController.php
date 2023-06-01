9c4458996b7b86a6bd150873698291b012d48c91
blacksou <7042phpdev>
2023-06-01 05:44:29 +0300

    add validate email GS-2

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -4,5 +4,16 @@ namespace App\Controllers;
 
 class BaseController
 {
+    protected function jsonResponse($data, $status_code = 200)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status_code);
+        echo json_encode($data);
+        exit();
+    }
 
+    protected function validateEmail($email)
+    {
+        return !filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
 }
